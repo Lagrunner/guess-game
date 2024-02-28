7f2a3d686e10886370b68de9da578a6ce2047412
Louis H P Cheung <cheunghangpui@gmail.com>
2024-02-29 01:05:28 +0800

    Added attempt counter

--- guessgame.py
+++ guessgame.py
@@ -2,13 +2,13 @@ from random import *
 win = False
 start_num = 1
 end_num = 99
-#attemp
+attempt = 0
 
 def board():    
     print("  ---------------")
     print(start_num, "      ?      ", end_num)
     print("  ---------------")
-
+# Print the game board
 '''
 def error_check(answer):
     
@@ -17,11 +17,14 @@ def error_check(answer):
         answer = int(input("Try again!!  "))
         
 '''
+# function to check number is out of range
+
 def answer_checker(answer):
 
     global start_num
     global end_num
     global win
+    global attempt
 
     if answer < x_number:
         start_num = answer
@@ -31,14 +34,18 @@ def answer_checker(answer):
     if answer == x_number:
         win = True
         print("Congratuation!! the answer is ", x_number)
+        print("You have tried", attempt, "times!!")
         exit()
 
 board()
+# Check the answer
+
 
 x_number = randint(1, 100)
 answer = int(input("Guess the answer!  "))
 while win != True:
     answer_checker(answer)
+    attempt += 1
     board()
     answer = int(input("The number is between " + str(start_num) + " and " + str(end_num) + " :  "))
 #    error_check(answer)
